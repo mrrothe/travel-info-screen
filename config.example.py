cd0f5118240ffb9b45cfa0f8dc3d916cc9a4f59a
martin <martin@rothe.uk>
2019-08-12 21:16:19 +0100

    Add example config

---
+++ config.example.py
@@ -0,0 +1,34 @@
+# API KEYS
+LDB_TOKEN = ""
+tfl_appid=""
+tfl_appkey=""
+
+# Branding
+logosrc="//cdn.com/images/logo.png"
+headercolor="1A4E7C"
+grad1="1A7C73"
+grad2="1A747C "
+companyname="Company Ltd"
+
+# Pre-populated stations
+commonstations={}
+tram_stations={
+    'Lace market':'9400ZZNOLCM',
+    'Nottingham Station':'9400ZZNONOT',
+    'Toton Lane':'9400ZZNOTOT',
+    'Phoenix Park':'9400ZZNOPHP',
+}
+bus_stations={
+    'Market Square':'123abc',
+}
+train_stations={
+    'Nottingham':'not',
+    'Derby':'dby',
+    'Northampton':'nmp',
+    'Normanton (Wakefield)':'nor',
+    'Liverpool Street (Mainline rail services)':'lst',
+    'Moorgate':'Mog',
+    'Kings Cross':'kgx',
+    'St Pancras International':'stp'
+    }
+
